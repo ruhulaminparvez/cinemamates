6cbab895f78a202f995e03e8b86f4e24ba1655d4
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-03-08 17:29:06 +0600

    Basic Counter :snake:

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 const App = () => {
     const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-        <button onClick={() => setCounter((prevCounter => prevCounter - 1))}>-</button>
-        <h1>{counter}</h1>
-        <button onClick={() => setCounter((prevCounter => prevCounter + 1))}>+</button>
+        <div className="btn">
+            <button onClick={() => setCounter((prevCounter => prevCounter - 1))}>-</button>
+            <h1 className="txt">{counter}</h1>
+            <button onClick={() => setCounter((prevCounter => prevCounter + 1))}>+</button>
+        </div>
+
     </div>
   );
 }
